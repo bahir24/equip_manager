dcbb66db630671790a8a9cd0bb444aac59437b38
equip-manager <7074033@gmail.com>
2019-07-22 23:00:49 +0300

    initial commit

--- src/scripts/feedback.js
+++ src/scripts/feedback.js
@@ -3,6 +3,8 @@ const sendButton = document.querySelector('.btn--form');
 
 sendButton.addEventListener('click', function (event) {
   event.preventDefault();
+
+  if (validateForm(form)) {
   var data = {
     email: form.elements.email.value,
     name: form.elements.name.value,
@@ -14,3 +16,25 @@ sendButton.addEventListener('click', function (event) {
   xhr.open('POST', 'robot.php');
   xhr.send(JSON.stringify(data));
 });
+
+function validateForm(form) {
+  let valid = true;
+  if (!validateField(form.elements.name)) {
+    valid = false;
+  }
+  if (!validateField(form.elements.email)) {
+    valid = false;
+  }
+  if (!validateField(form.elements.tel)) {
+    valid = false;
+  }
+  if (!validateField(form.elements.services)) {
+    valid = false;
+  }
+  return valid;
+}
+
+function validateField(field) {
+  field.nextElementSibling.textContent = field.validationMessage;
+  return field.checkValidity();
+}
